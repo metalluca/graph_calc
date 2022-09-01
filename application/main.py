2f4d78f20790eabbbca480cf7189adda8da0b0cf
luca <luca.bonn@alumni.fh-aachen.de>
2022-09-01 20:46:17 +0200

    Implemented some sympy functionality for the symbolic part

--- application/main.py
+++ application/main.py
@@ -1,6 +1,15 @@
+from ast import parse
+from code import interact
 from turtle import left
 import matplotlib.pyplot as plt
 import numpy as np
+from sympy.parsing.sympy_parser import parse_expr
+from sympy.solvers import solve
+from sympy import symbols
+from sympy import Lt, Gt
+from sympy.plotting import plot 
+from sympy.plotting import plot_implicit
+from sympy import Rel
 
 class linear_function():
     def __init__(self, m, b):
@@ -13,7 +22,6 @@ class linear_function():
     def plof_lf(self, x):
         y = self.compute_y(x)
         plt.plot(x, y, label="y="+str(self.m)+"x"+str(self.b))
-        # TODO: Adjust the right inequality conditon for space filling
         plt.fill_between(
                     x=x,
                     y1=y,
@@ -57,16 +65,27 @@ def create_fig():
 def main():
     print("--- Welcome to the LP Visualizer ---")
     # zf = input("Enter Zielfunktion (e.g. maximize 3x+4y): ")
-    nb1 = input("Enter Nebenbedingung 1 solved for y (e.g. y<=-2*x+4): ")
+    # expr = input("Enter Nebenbedingung: ")
+    expr = "1*x1 + 4*x2 <= 20"
+    x1, x2 = symbols('x1 x2')
+    # eq = parse_expr(expr)
+    # res = solve(eq, x2)
+    X = np.linspace(-10, 10, 10)
+    x1, x2 = symbols('x1 x2')
+    eq = Rel(1*x1 + 4*x2, 20, "==")
+    res = solve(eq, x2)
+    intercept = res[0].as_coeff_Add()[0]
+    slope = res[0].as_coeff_Add()[1]
     # nb2 = input("Enter Nebenbedingung 2 solved for y (e.g. y<=-1/2*x+4): ")
     # nb3 = input("Enter Nebenbedingung 3 solved for y (e.g. y<=-1/2*x+4): ")
-    lf1 = linear_function(*get_params(nb1))
+    lf1 = linear_function(intercept, slope)
+    print(lf1.b, lf1.m)
+    print(lf1.compute_y(X))
     # lf2 = linear_function(*get_params(nb2))
     # lf3 = linear_function(*get_params(nb3))
-    X = np.linspace(-10, 10, 10)
     
     ax, fig = create_fig()
-    lf1.plof_lf(X)
+    # lf1.plof_lf(X)
     plt.grid()
     plt.gca().set_aspect("equal")
     plt.legend(loc="upper left")
